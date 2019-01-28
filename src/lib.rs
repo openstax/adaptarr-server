b7aef3d7b52a3226b13b8cee7db693ef67ee8c3a
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-01-28 12:20:57 +0100

    Add log for better logging

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ extern crate zip;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate failure_derive;
 #[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate tera;
 
