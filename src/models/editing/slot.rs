0acba310a0ee3a8ea771081ea7bd7c6f6876225b
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-04-29 15:07:54 +0200

    Add Slot::fill

--- src/models/editing/slot.rs
+++ src/models/editing/slot.rs
@@ -1,6 +1,16 @@
+use diesel::{
+    prelude::*,
+    result::Error as DbError,
+};
 use std::ops::Deref;
+use uuid::Uuid;
 
-use crate::db::models as db;
+use crate::db::{
+    Connection,
+    models as db,
+    schema::{draft_slots, users},
+    functions::count_distinct,
+};
 
 /// Abstract representation of roles a user can take during an editing process.
 #[derive(Debug)]
@@ -13,6 +23,34 @@ impl Slot {
     pub(super) fn from_db(data: db::EditProcessSlot) -> Slot {
         Slot { data }
     }
+
+    /// Fill this slot with a user for a particular draft.
+    pub fn fill(&self, dbcon: &Connection, draft: Uuid)
+    -> Result<Option<i32>, FillSlotError> {
+        if !self.data.autofill {
+            return Ok(None);
+        }
+
+        let user = users::table
+            .inner_join(draft_slots::table)
+            .select(users::all_columns)
+            .filter(users::role.eq(self.data.role))
+            .group_by((users::all_columns, draft_slots::draft))
+            .order_by(count_distinct(draft_slots::draft).asc())
+            .first::<db::User>(dbcon)
+            .optional()?
+            .ok_or(FillSlotError::NoUser)?;
+
+        diesel::insert_into(draft_slots::table)
+            .values(db::DraftSlot {
+                draft,
+                slot: self.data.id,
+                user: user.id,
+            })
+            .execute(dbcon)?;
+
+        Ok(Some(user.id))
+    }
 }
 
 impl Deref for Slot {
@@ -21,4 +59,20 @@ impl Deref for Slot {
     fn deref(&self) -> &db::EditProcessSlot {
         &self.data
     }
+}
+
+#[derive(ApiError, Debug, Fail)]
+pub enum FillSlotError {
+    /// Database error
+    #[fail(display = "Database error: {}", _0)]
+    #[api(internal)]
+    Database(#[cause] DbError),
+    /// There is no user available to fill this slot
+    #[fail(display = "There is no user available to fill this slot")]
+    #[api(code = "edit-process:slot:fill")]
+    NoUser,
+}
+
+impl_from! { for FillSlotError ;
+    DbError => |e| FillSlotError::Database(e),
 }
