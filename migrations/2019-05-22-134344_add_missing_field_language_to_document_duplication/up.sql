906aebaba13126666aaa2a294ea3c6549c54c5c8
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-05-23 13:31:26 +0200

    Fix drafts not inheriting parent module's language

---
+++ migrations/2019-05-22-134344_add_missing_field_language_to_document_duplication/up.sql
@@ -0,0 +1,20 @@
+create or replace function duplicate_document(old_id integer)
+returns integer
+as $$
+declare
+    new_id integer;
+begin
+    insert into documents (title, index, language)
+    select title, index, language
+    from documents
+    where id = old_id
+    returning id into new_id;
+
+    insert into document_files (document, name, file)
+    select new_id, name, file
+    from document_files
+    where document = old_id;
+
+    return new_id;
+end
+$$ language plpgsql
